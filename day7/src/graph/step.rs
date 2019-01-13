3d527ae6ed087c04a3581a6474388d5716584aa5
Anton Holmberg <anton.holmberg@greenely.se>
2019-01-13 20:20:11 +0100

    Refactor to use str

--- day7/src/graph/step.rs
+++ day7/src/graph/step.rs
@@ -4,45 +4,48 @@ use regex::Regex;
 use std::collections::HashSet;
 
 #[derive(PartialEq, Debug)]
-pub struct Step {
-    name: String,
-    children: HashSet<String>
+pub struct Step<'a> {
+    name: &'a str,
+    children: HashSet<&'a str>,
 }
 
-impl Step {
-    pub fn new(name: String) -> Step {
+impl<'a> Step<'a> {
+    pub fn new(name: &'a str) -> Step<'a> {
         Step {
             name: name,
             children: HashSet::new(),
         }
     }
 
-    fn add_child(&mut self, child: String) {
+    fn add_child(&mut self, child: &'a str) {
         self.children.insert(child);
     }
 
-    pub fn merge(mut self, other: Step) -> Result<Step, String> {
-        if (self.name != other.name) {
-            return Err(String::from("Can not merge nodes of different name"));
+    pub fn merge(mut self, other: Step<'a>) -> Result<Step, &'a str> {
+        if self.name != other.name {
+            return Err("Can not merge nodes of different name");
         }
 
-        for item in other.children.into_iter() {
+        for item in other.children.iter() {
             self.children.insert(item);
         }
 
-
-        Ok(Step { name: self.name, children: self.children})
+        Ok(Step {
+            name: self.name,
+            children: self.children,
+        })
     }
 }
 
-impl<'a> From<String> for Step {
-    fn from(input: String) -> Step {
-        let re = Regex::new(r"^Step ([A-Z]) must be finished before step ([A-Z]) can begin.$").unwrap();
+impl<'a> From<&'a str> for Step<'a> {
+    fn from(input: &'a str) -> Step<'a> {
+        let re =
+            Regex::new(r"^Step ([A-Z]) must be finished before step ([A-Z]) can begin.$").unwrap();
 
-        let captures = re.captures(input.as_str()).unwrap();
-        let mut step = Step::new(captures[1].to_string());
+        let captures = re.captures(input).unwrap();
+        let mut step = Step::new(captures.get(1).unwrap().as_str());
 
-        step.add_child(captures[2].to_string());
+        step.add_child(captures.get(2).unwrap().as_str());
         return step;
     }
 }
@@ -50,28 +53,38 @@ impl<'a> From<String> for Step {
 #[cfg(test)]
 mod test {
     use super::Step;
-    use std::collections::HashSet;
-    use std::iter::FromIterator;
 
     #[test]
     fn test_step_from() {
-        let step = Step::from("Step G must be finished before step T can begin.".to_string());
+        let step = Step::from("Step G must be finished before step T can begin.");
         assert_eq!(
             step,
             Step {
-                name: "G".to_string(),
-                children: HashSet::from_iter(vec!["T".to_string()].into_iter())
+                name: "G",
+                children: ["T"].iter().cloned().collect()
             }
         );
     }
 
     #[test]
     fn test_merge() {
-        let original = Step { name: "G".to_string(), children: HashSet::from_iter(vec!["T".to_string()].into_iter()) };
-        let to_merge = Step { name: "G".to_string(), children: HashSet::from_iter(vec!["H".to_string()].into_iter()) };
+        let original = Step {
+            name: "G",
+            children: ["T"].iter().cloned().collect(),
+        };
+        let to_merge = Step {
+            name: "G",
+            children: ["H"].iter().cloned().collect(),
+        };
 
         let merged = original.merge(to_merge).expect("Could not merge");
 
-        assert_eq!(merged, Step { name: "G".to_string(), children: HashSet::from_iter(vec!["T".to_string(), "H".to_string()].into_iter()) });
+        assert_eq!(
+            merged,
+            Step {
+                name: "G",
+                children: ["T", "H"].iter().cloned().collect()
+            }
+        );
     }
 }
